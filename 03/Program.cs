3ff795dccd7b87704d251e37bff9de7230c41540
Oleg <olegashepelev@gmail.com>
2023-01-13 14:19:06 +0600

    Complete third task

---
+++ 03/Program.cs
@@ -0,0 +1,18 @@
+﻿GetRandomNum();
+
+void GetRandomNum()
+{
+    Console.Write("[");
+    int[] array = new int[8];
+
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+        Console.Write(array[i]);
+        if (i < 7)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
