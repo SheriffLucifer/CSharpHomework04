084c657516bf305821bc6545da6ffd6c210b5d6f
Oleg <olegashepelev@gmail.com>
2023-01-13 12:58:46 +0600

    Complete first task

---
+++ 01/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int firstNumber = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+int secondNumber = GetNumberFromUser("Введите целое число B: ", "Ошибка ввода!");
+int getNumber = GetNumber(firstNumber, secondNumber);
+Console.WriteLine($"{firstNumber}, {secondNumber} -> {getNumber}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetNumber(int firstNumber, int secondNumber)
+{
+    int result = 1;
+
+    for (int i = 1; i <= secondNumber; i++)
+    {
+        result = result * firstNumber;
+    }
+    return result;
+}
