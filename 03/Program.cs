b95324f96e8cc5c36aea00fb1b7bcd25f772a68d
Oleg <olegashepelev@gmail.com>
2023-01-16 19:33:58 +0600

    Correct third task

--- 03/Program.cs
+++ 03/Program.cs
@@ -5,11 +5,11 @@ void GetRandomNum()
     Console.Write("[");
     int[] array = new int[8];
 
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         array[i] = new Random().Next(0, 100);
         Console.Write(array[i]);
-        if (i < 7)
+        if (i < array.Length - 1)
         {
             Console.Write(", ");
         }
